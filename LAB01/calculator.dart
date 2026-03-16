6fc9fc9816ad1e4ff3a8aff309b77cb52452dd4d
Tia <septiaaisnaeni@gmail.com>
2026-03-16 17:35:21 +0700

    Create calculator.dart

---
+++ LAB01/calculator.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 6;
+  int b = 7;
+
+  print("Exercise 1");
+  print("=" * 10);
+  print("Penjumlahan: ${a + b}");
+  print("Pengurangan: ${a - b}");
+  print("Perkalian: ${a * b}");
+  print("Pembagian: ${a / b}");
+}

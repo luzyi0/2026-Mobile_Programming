94daeea9d11016f78452ad962ef47cb2c01ae4c2
Tia <septiaaisnaeni@gmail.com>
2026-03-16 17:39:28 +0700

    Create luas_persegi_panjang.dart

---
+++ LAB01/luas_persegi_panjang.dart
@@ -0,0 +1,10 @@
+int luasPersegi(int panjang, int lebar) {
+  return panjang * lebar;
+}
+
+void main() {
+  int area = luasPersegi(6, 7);
+  print("Challenge Task");
+  print("=" * 15);
+  print("Luas Persegi: $area");
+}

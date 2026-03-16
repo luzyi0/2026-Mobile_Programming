ea06aa9969a21dd029b2adf8bf86d80dce34e606
Tia <septiaaisnaeni@gmail.com>
2026-03-16 17:37:35 +0700

    Create ganjil_genap.dart

---
+++ LAB01/ganjil_genap.dart
@@ -0,0 +1,11 @@
+void main() {
+  int angka = 67;
+
+  print("Evercise 2");
+  print("=" * 10);
+  if (angka % 2 == 0) {
+    print("$angka merupakan bilangan genap");
+  } else {
+    print("$angka merupakan bilangan ganjil");
+  }
+}

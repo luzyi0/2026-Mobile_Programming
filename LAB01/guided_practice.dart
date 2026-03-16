6e2aebd5c2f2d9b8fe0e5f4b0baad866849026b3
Tia <septiaaisnaeni@gmail.com>
2026-03-16 17:37:05 +0700

    Create guided_practice.dart

---
+++ LAB01/guided_practice.dart
@@ -0,0 +1,15 @@
+void main() {
+  String nama = "Tia";
+  String nim = "4.33.24.1.25";
+  String prodi = "D-4 Teknologi Rekayasa Komputer";
+  String jurusan = "Teknik Elektro";
+  String kampus = "Politeknik Negeri Semarang";
+
+  print("Guided Practice");
+  print("=" * 15);
+  print("Nama: $nama");
+  print("NIM: $nim");
+  print("Prodi: $prodi");
+  print("Jurusan: $jurusan");
+  print("Institusi: $kampus");
+}
